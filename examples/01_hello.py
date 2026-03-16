c2f6d128dbb2ecfb581034ef13e5539e07429492
Amir <amir.h.bakhtiary@gmail.com>
2026-03-16 12:07:58 +0100

    rename examples

--- examples/01_hello.py
+++ examples/01_hello.py
@@ -1,7 +1,7 @@
 from popa.agent import create_simple_agent
 
 if __name__ == '__main__':
-    agent = create_simple_agent("You are a helpful greeter. Great incoming people.")
+    agent = create_simple_agent("You are a helpful greeter. Greet incoming people.")
 
     result = agent.ask("A man arrives what do you say to him?")
 
